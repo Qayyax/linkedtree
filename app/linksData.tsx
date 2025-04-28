0fac22c342e5461873cab8c3ea5bc261604ec5ab
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-04-28 17:49:24 -0400

    added linkedin and text

--- app/linksData.tsx
+++ app/linksData.tsx
@@ -5,23 +5,14 @@ import { FaXTwitter as Twitter } from "react-icons/fa6";
 import { FaYoutube as Youtube } from "react-icons/fa6";
 import { FaGithub as Github } from "react-icons/fa";
 import { FaCode as Code } from "react-icons/fa";
+import { FaLinkedin as LinkedIn } from "react-icons/fa6";
 
 // time to get the links I guess
 export const linksData: linkProperty[] = [
   {
-    title: "Instagram",
-    link: "https://www.instagram.com/tife_len/",
-    icon: Instagram,
-  },
-  {
-    title: "X / Twitter",
-    link: "https://x.com/qayyax",
-    icon: Twitter,
-  },
-  {
     title: "Youtube - Vlog",
     link: "https://www.youtube.com/@TifeLogs",
-    description: "I post weekly activities here and sometimes I stream",
+    description: "I post weekly activities here",
     icon: Youtube,
   },
   {
@@ -40,5 +31,20 @@ export const linksData: linkProperty[] = [
     link: "https://www.qayyax.xyz",
     description: "Where I post projects I have worked on",
     icon: Code,
+  },
+  {
+    title: "LinkedIn",
+    link: "https://www.linkedin.com/in/tife-olatunji-b12729296/",
+    icon: LinkedIn,
+  },
+  {
+    title: "Instagram",
+    link: "https://www.instagram.com/tife_len/",
+    icon: Instagram,
+  },
+  {
+    title: "X / Twitter",
+    link: "https://x.com/qayyax",
+    icon: Twitter,
   },
 ];
--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,13 @@ import { shantellSans } from "./fonts";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center gap-4 pt-10">
-      <div className="name-link flex flex-col gap-2 mb-2">
+      <div className="name-link flex flex-col gap-2 mb-2 items-center justify-center">
         <h1 className={`${shantellSans.className} text-3xl font-extrabold`}>
           Tife Olatunji
         </h1>
+        <div className={`text-center  ${shantellSans.className}`}>
+          <p>I am currently looking for junior developer roles by the way</p>
+        </div>
         {/* Component goes here */}
         <LinkListSmall />
       </div>
