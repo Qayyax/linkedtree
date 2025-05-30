eff4c1c83979c5de606b9d4bcc8072d61dd915ea
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-05-29 20:23:51 -0400

    added margin to the bottom of page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { shantellSans } from "./fonts";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center gap-4 pt-10">
+    <main className="flex flex-col items-center justify-center gap-4 pt-10 mb-5">
       <div className="name-link flex flex-col gap-2 mb-2 items-center justify-center">
         <h1 className={`${shantellSans.className} text-3xl font-extrabold`}>
           Tife Olatunji
